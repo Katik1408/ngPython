ee1d58a4834150416139e8dea3e62af3333b4cef
Karthik Saxena <katik.ks@gmail.com>
2021-03-12 10:44:08 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
 
+//Decorator
+
 @Component({
-  selector: 'app-root',
+  selector: 'root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'students';
+  title = 'Main Title';
+  a: number = 5;
+  isActive = false;
+  max: number = 10;
+  rate: number = 7;
+  isReadonly: boolean = true;
+  isSpecial = false;
+  // url = 'assets\\images\\bike.jpg'
+  getData() {
+    return 'This is from function';
+  }
+  changeData() {
+    this.title = 'New Title';
+  }
+
+  students = [
+    {
+      name: 'Rahul',
+      Age: 25,
+      place: 'Bangalore',
+    },
+    { name: 'Rohit', Age: 25, place: 'Delhi' },
+    {
+      name: 'Harsh',
+      Age: 24,
+      place: 'Noida',
+    },
+    {
+      name: 'Venkat',
+      Age: 24,
+      place: 'Noida',
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CategoryComponent } from 'src/app/category/category.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BasicComponent } from 'src/app/basic/basic.component';
+import { FormsModule } from '@angular/forms';
+import { FirstComponent } from './first/first.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { RatingModule } from 'ngx-bootstrap/rating';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    NavbarComponent,
+    BasicComponent,
+    FirstComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatProgressBarModule,
+    RatingModule.forRoot(),
+    MatButtonModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
